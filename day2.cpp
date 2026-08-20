b00a6be714940c925a03d844de70a176cdf97eba
Anand <anand7451kumar@icloud.com>
2026-08-20 10:36:10 +0530

    Implement basic arithmetic operations in day2.cpp
    Prints the sum, difference, and product of two integers.

---
+++ day2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+
+    cout << "Sum: " << a + b << endl;
+    cout << "Difference: " << a - b << endl;
+    cout << "Product: " << a * b << endl;
+
+    return 0;
+}
